8c41b8d90cd8eaa1945dffce9b0fd2536ad0e22e
sremedios <sam.remedios@gmail.com>
2020-09-12 02:57:17 -0400

    ood implemented, model trains

--- models/unet.py
+++ models/unet.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch
 
 class ConvBlock(nn.Module):
     def __init__(
@@ -39,34 +40,104 @@ class UNet(nn.Module):
         self,
         n_channels,
         kernel_size=3,
-        n_layers=67,
         ds=1,
     ):
-        padding = kernel_size // 2
         super(UNet, self).__init__()
-        
+
+        padding = kernel_size // 2
+
+
         self.downsample = nn.MaxPool2d(kernel_size=2)
-        self.upsample = nn.MaxUnpool2d(kernel_size=2)
-
-
-        self.down_1 = ConvBlock(in_channels=n_channels,out_channels=64//ds,padding=padding)
-        self.down_2 = ConvBlock(in_channels=64//ds,out_channels=128//ds,padding=padding)
-        self.down_3 = ConvBlock(in_channels=128//ds,out_channels=256//ds,padding=padding)
-        self.down_4 = ConvBlock(in_channels=256//ds,out_channels=512//ds,padding=padding)
         
-        self.bn = ConvBlock(in_channels=512//ds,out_channels=1024//ds,padding=padding)
+        self.down_1 = ConvBlock(
+            in_channels=n_channels,
+            out_channels=64//ds,
+            kernel_size=kernel_size,
+            padding=padding,
+        )
+        self.down_2 = ConvBlock(
+            in_channels=64//ds,
+            out_channels=128//ds,
+            padding=padding,
+        )
+        self.down_3 = ConvBlock(
+            in_channels=128//ds,
+            out_channels=256//ds,
+            kernel_size=kernel_size,
+            padding=padding,
+        )
+        self.down_4 = ConvBlock(
+            in_channels=256//ds,
+            out_channels=512//ds,
+            kernel_size=kernel_size,
+            padding=padding,
+        )
+
+        self.bn = ConvBlock(
+            in_channels=512//ds,
+            out_channels=1024//ds,
+            kernel_size=kernel_size,
+            padding=padding,
+        )
+
+        self.upsample_4 = nn.ConvTranspose2d(
+            in_channels=1024//ds,
+            out_channels=512//ds,
+            kernel_size=2,
+            stride=2,
+        )
+        self.up_4 = ConvBlock(
+            in_channels=1024//ds,
+            out_channels=512//ds,
+            kernel_size=kernel_size,
+            padding=padding,
+        )
         
-        self.up_4 = ConvBlock(in_channels=1024//ds,out_channels=512//ds,padding=padding)
-        self.up_3 = ConvBlock(in_channels=512//ds,out_channels=256//ds,padding=padding)
-        self.up_2 = ConvBlock(in_channels=256//ds,out_channels=128//ds,padding=padding)
-        self.up_1 = ConvBlock(in_channels=128//ds,out_channels=64//ds,padding=padding)
+        self.upsample_3 = nn.ConvTranspose2d(
+            in_channels=512//ds,
+            out_channels=256//ds,
+            kernel_size=2,
+            stride=2,
+        )
+        self.up_3 = ConvBlock(
+            in_channels=512//ds,
+            out_channels=256//ds,
+            kernel_size=kernel_size,
+            padding=padding,
+        )
+        
+        self.upsample_2 = nn.ConvTranspose2d(
+            in_channels=256//ds,
+            out_channels=128//ds,
+            kernel_size=2,
+            stride=2,
+        )
+        self.up_2 = ConvBlock(
+            in_channels=256//ds,
+            out_channels=128//ds,
+            kernel_size=kernel_size,
+            padding=padding,
+        )
+        
+        self.upsample_1 = nn.ConvTranspose2d(
+            in_channels=128//ds,
+            out_channels=64//ds,
+            kernel_size=2,
+            stride=2,
+        )
+        self.up_1 = ConvBlock(
+            in_channels=128//ds,
+            out_channels=64//ds,
+            kernel_size=kernel_size,
+            padding=padding,
+        )
+        
         
         self.seg = nn.Sequential(
             nn.Conv2d(
                 in_channels=64//ds,
                 out_channels=n_channels,
                 kernel_size=1,
-                padding=padding,
                 stride=1,
                 bias=True,
             ),
@@ -77,15 +148,15 @@ class UNet(nn.Module):
     def encode(self, x):
         stage_1 = self.down_1(x)
         stage_2 = self.down_2(self.downsample(stage_1))
-        stage_3 = self.down_2(self.downsample(stage_2))
-        stage_4 = self.down_2(self.downsample(stage_3))
-        
-        bn = self.bn(stage_4)
-        
-        stage_4 = self.up_4(torch.cat([bn, self.upsample(stage_4)], dim=2))
-        stage_3 = self.up_3(torch.cat([stage_3, self.upsample(stage_4)], dim=2))
-        stage_2 = self.up_2(torch.cat([stage_2, self.upsample(stage_3)], dim=2))
-        stage_1 = self.up_1(torch.cat([stage_1, self.upsample(stage_2)], dim=2))
+        stage_3 = self.down_3(self.downsample(stage_2))
+        stage_4 = self.down_4(self.downsample(stage_3))
+
+        bn = self.bn(self.downsample(stage_4))
+
+        stage_4 = self.up_4(torch.cat([stage_4, self.upsample_4(bn)], dim=1))
+        stage_3 = self.up_3(torch.cat([stage_3, self.upsample_3(stage_4)], dim=1))
+        stage_2 = self.up_2(torch.cat([stage_2, self.upsample_2(stage_3)], dim=1))
+        stage_1 = self.up_1(torch.cat([stage_1, self.upsample_1(stage_2)], dim=1))
         
         return stage_1
     
--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,5 +1,6 @@
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
+import torch
 import nibabel as nib
 import numpy as np
 
@@ -7,11 +8,11 @@ class CTSlices(Dataset):
     def __init__(self, ct_dir, mask_dir):
         self.ct_fpaths = sorted(ct_dir.iterdir())
         self.mask_fpaths = sorted(mask_dir.iterdir())
-        self.norm = Normalize()
+        self.norm = Normalize(hu_min=-1000, hu_max=100)
         self.pos = True
         
     def __len__(self):
-        return len(self.ct_fpaths)
+        return len(self.mask_fpaths)
     
     def __getitem__(self, i):
         ct = nib.load(self.ct_fpaths[i]).get_fdata(dtype=np.float32)
@@ -19,9 +20,9 @@ class CTSlices(Dataset):
         
         ct = self.norm(ct)
         ct_slice, mask_slice = get_slice(ct, mask, self.pos)
-        self.pos = not self.pos
+#         self.pos = not self.pos
         
-        return ct_slice, mask_slice
+        return ct_slice.cuda(), mask_slice.cuda()
     
 def get_slice(ct, mask, is_pos):
     i = np.random.randint(0, ct.shape[-1])
@@ -32,17 +33,24 @@ def get_slice(ct, mask, is_pos):
     else:
         while mask[..., i].sum() != 0:
             i = np.random.randint(0, mask.shape[-1])
-    return ct[..., i], mask[..., i]
+            
+    ct = torch.from_numpy(ct[np.newaxis, ..., i]).float()
+    mask = torch.from_numpy(mask[np.newaxis, ..., i]).float()
+    return ct, mask
 
 
 class Normalize:
-    def __init__(self):
+    def __init__(self, hu_min, hu_max):
         self.squash_min = 0
         self.squash_max = 0
         self.orig_mean = 0
         self.orig_std = 0
+        self.hu_min = hu_min
+        self.hu_max = hu_max
     
     def __call__(self, x):
+        x[np.where(x < self.hu_min)] = self.hu_min
+        x[np.where(x > self.hu_max)] = self.hu_max
         self.orig_mean = x.mean()
         self.orig_std = x.std()
         x = (x - self.orig_mean) / self.orig_std
